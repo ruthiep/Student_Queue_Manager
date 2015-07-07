a3503c8c4d102abce9adbdb58a42a2b69f012805
Ruthie Peters <ruthie_peters@yahoo.com>
2015-07-07 13:37:22 -0500

    more updates

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,1 @@
+before_filter :require_login
---
+++ db/migrate/20150706184150_create_user_table.rb
@@ -0,0 +1,10 @@
+class CreateUserTable < ActiveRecord::Migration
+  # def change
+#     create_table :user_tables do |t|
+#       t.string :name
+#
+#     end
+#
+#   end
+  
+end
