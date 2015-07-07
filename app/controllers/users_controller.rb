f0a8b30427668647bf4749e1cd76e36c35eb6c3e
Ruthie Peters <ruthie_peters@yahoo.com>
2015-07-07 16:39:16 -0500

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+           :password_confirmation, :crypted_password, :salt )
   end
 end
