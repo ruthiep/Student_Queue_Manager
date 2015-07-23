dcff6fe6a5bdf080cc54358e9963c05e77284b21
Ruthie Peters <ruthie_peters@yahoo.com>
2015-07-23 11:51:25 -0500

    dropdown box and instructions added, auto-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to '/users', :notice => "Signed up!"
+      auto_login(@user)
+      redirect_to '/questions', :notice => "Signed up!"
     else
       render :new
     end
